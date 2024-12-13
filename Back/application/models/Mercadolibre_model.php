7403388fe5837ad9190e1d795f7c17d9d1a818ba
David <rexx84@gmail.com>
2024-12-13 19:47:49 -0300

    Update 12122024

--- Back/application/models/Mercadolibre_model.php
+++ Back/application/models/Mercadolibre_model.php
@@ -317,8 +317,61 @@ class Mercadolibre_model extends CI_Model
 		}
 	}
 
+	public function validateSignatureKeyMP($response){
+		// Obtain the x-signature value from the header
+		$xSignature = $_SERVER['HTTP_X_SIGNATURE'];
+		$xRequestId = $_SERVER['HTTP_X_REQUEST_ID'];
+
+		// Obtain Query params related to the request URL
+		$queryParams = $_GET;
+
+		// Extract the "data.id" from the query params
+		$dataID = isset($queryParams['data.id']) ? $queryParams['data.id'] : '';
+
+		// Separating the x-signature into parts
+		$parts = explode(',', $xSignature);
+
+		// Initializing variables to store ts and hash
+		$ts = null;
+		$hash = null;
+
+		// Iterate over the values to obtain ts and v1
+		foreach ($parts as $part) {
+			// Split each part into key and value
+			$keyValue = explode('=', $part, 2);
+			if (count($keyValue) == 2) {
+				$key = trim($keyValue[0]);
+				$value = trim($keyValue[1]);
+				if ($key === "ts") {
+					$ts = $value;
+				} elseif ($key === "v1") {
+					$hash = $value;
+				}
+			}
+		}
+
+		// Obtain the secret key for the user/application from Mercadopago developers site
+		$secret = "your_secret_key_here";
+
+		// Generate the manifest string
+		$manifest = "id:$dataID;request-id:$xRequestId;ts:$ts;";
+
+		// Create an HMAC signature defining the hash type and the key as a byte array
+		$sha = hash_hmac('sha256', $manifest, $secret);
+		if ($sha === $hash) {
+			// HMAC verification passed
+			echo "HMAC verification passed";
+		} else {
+			// HMAC verification failed
+			echo "HMAC verification failed";
+		}
+	}
 	public function getNotificationFromMP($response)
 	{
+		print_r($response);
+		$cyphedSignature = hash_hmac('sha256', $data, $key);
+
+		# aec583f7ded6598f8548107537981444727d437588ba666e59396bde1e0039e5
 		//var_dump($response['api_version']);
 		// ENVIAMOS EL MAIL DE CONFIRMAR REGISTRO //
 		/*MAIL*/
