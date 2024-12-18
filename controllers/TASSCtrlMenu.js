9912a0cd98a015a19d821e811d0da091300451f4
David <rexx84@gmail.com>
2024-12-18 02:07:01 -0300

    Update 18122024

--- controllers/TASSCtrlMenu.js
+++ controllers/TASSCtrlMenu.js
@@ -771,7 +771,7 @@
                   }
                 }
               }
-          ); 
+              ); 
               $('.input--depto').mask('ZZZ',
                   {
                     translation:{
