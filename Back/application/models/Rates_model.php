4d16bfbc0dda4166400f80b0de0ac26c96a0e02e
David <rexx84@gmail.com>
2025-01-03 21:23:21 -0300

    Update 03012025

--- Back/application/models/Rates_model.php
+++ Back/application/models/Rates_model.php
@@ -85,7 +85,11 @@ class Rates_model extends CI_Model {
         $this->db->join('tb_technician_services_type', 'tb_technician_services_type.idServiceType = tb_technician_services.idServiceTypeFk', 'left');
         $this->db->join('tb_technician_services_mode', 'tb_technician_services_mode.idServiceMode = tb_technician_services.idServiceModeFk', 'left');
         $this->db->where('tb_technician_services.idServiceTypeFk', $item['idServiceTechnician']);
-        $this->db->where('tb_technician_services.idServiceModeFk', $item['deviceIsOnline']);
+        if ($item['hasStock']=='0'){
+            $this->db->where('tb_technician_services.idServiceModeFk', 2);
+        }else{
+            $this->db->where('tb_technician_services.idServiceModeFk', $item['deviceIsOnline']);
+        }
         $this->db->where('tb_technician_services.hasStock', $item['hasStock']);
         if ($item['hasStock']=='0'){
             $this->db->like('tb_technician_services.description', "ALTA-SIN-STOCK", 'both', false);
@@ -98,7 +102,7 @@ class Rates_model extends CI_Model {
         if ($query->num_rows() === 1) {
             // First, fetch the minimum cost for the given idServiceTechnicianKf
             if ($item['hasStock']=='0'){
-                #$this->db->select('MAX(cost) as min_cost');
+                $this->db->select('*');
             }else{
                 $this->db->select('MIN(cost) as min_cost');
             }
@@ -107,8 +111,10 @@ class Rates_model extends CI_Model {
             $subquery = $this->db->get()->row();
             //print_r($subquery);
             // Ensure that the subquery has returned a result
-            if ($subquery && $subquery->min_cost !== null) {
-                $min_cost = (float) $subquery->min_cost;
+            if (($subquery && isset($subquery->min_cost) && $subquery->min_cost !== null) || ($subquery && ! isset($subquery->min_cost))) {
+                if (($subquery && isset($subquery->min_cost) && $subquery->min_cost !== null)){
+                    $min_cost = (float) $subquery->min_cost;
+                }
                 $this->db->select("*");
                 $this->db->from("tb_contratos");
                 $this->db->join('tb_servicios_del_contrato_cabecera', 'tb_servicios_del_contrato_cabecera.idContratoFk = tb_contratos.idContrato', 'left');
@@ -136,12 +142,12 @@ class Rates_model extends CI_Model {
                     //$this->db->where('tb_servicios_del_contrato_cabecera.idServiceType', $item['idServiceType']);
                     $this->db->where('tb_technician_services.idServiceTypeFk', $item['idServiceType']);
                     $this->db->where('tb_technician_service_cost.idServiceTechnicianKf', $tb_technician_services->idServiceTechnician);
-                    if ($query2->num_rows()==1 || $item['hasStock']=='0'){
+                    if ($query2->num_rows()>=1 || $item['hasStock']=='0'){
                         //print($query2->num_rows());
                         $this->db->where('tb_technician_service_cost.idTipoMantenimientoKf', $contract['maintenanceType']);
-                    }else{
-                        $this->db->where('tb_technician_service_cost.cost', $min_cost); // Compare with the minimum cost
-                    }
+                    }#else{
+                    #   $this->db->where('tb_technician_service_cost.cost', $min_cost); // Compare with the minimum cost
+                    #
                     // Subquery for minimum cost                   
                     //$this->db->where('tb_technician_service_cost.cost = (SELECT MIN(cost + 0) AS min FROM tb_technician_service_cost WHERE tb_technician_service_cost.idTipoMantenimientoKf = "' . $contract['maintenanceType']. '" AND tb_technician_service_cost.idServiceTechnicianKf = "' . $tech_service['idServiceTechnician'] . '")', NULL, FALSE);
 
