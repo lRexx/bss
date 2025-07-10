f735cf13accc050e1268526955c8e58849b5aa0e
David <rexx84@gmail.com>
2025-07-10 14:00:01 -0300

    Update 10072025

--- controllers/TASSCtrlMonitor.js
+++ controllers/TASSCtrlMonitor.js
@@ -1436,13 +1436,13 @@ monitor.controller('MonitorCtrl', function($scope, $rootScope, $http, $location,
               ($scope.ticket.building!=undefined && $scope.ticket.building.isStockInOffice=='0' && $scope.ticket.building.isStockInBuilding=='0') || 
               ($scope.ticket.building!=undefined && $scope.ticket.building.allowOfficePickup!='1')){
               //console.log(item);
-              $scope.ticket.delivery.idTypeDeliveryKf="2";
+              //$scope.ticket.delivery.idTypeDeliveryKf="2";
               return item.idTypeDelivery != "1";
           }else{
               return item.idTypeDelivery;
           }
-          
       };
+
     /******************************
     *    UTIL FOR CUSTOMER DATA   *
     ******************************/
