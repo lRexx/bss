e3369fed5409956e123a143598e3198a83152e63
David <rexx84@gmail.com>
2025-08-26 17:28:43 -0300

    Update 26082025

--- Back/application/models/Mercadolibre_model.php
+++ Back/application/models/Mercadolibre_model.php
@@ -370,7 +370,7 @@ class Mercadolibre_model extends CI_Model
 				$lastPaymentAddedQuery = null;
 				$lastPaymentAddedQuery = $this->paymentById($response_decode->external_reference);
 				$lastPaymentAddedQuery_decode = json_decode(json_encode($lastPaymentAddedQuery[0]));
-				log_message('info', json_decode($lastPaymentAddedQuery[0]));
+				log_message('info', json_decode(json_encode($lastPaymentAddedQuery[0])));
 				if (is_null($lastPaymentAddedQuery_decode->mp_payment_id) || $lastPaymentAddedQuery_decode->mp_payment_id == 0) {
 					//print("El pago no se encuentra registado"); $response_decode->id;
 					$dataObj = null;
