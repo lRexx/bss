1396896fdfc800a4ea4b3aaff3722b8b9db56c64
David <rexx84@gmail.com>
2025-08-26 17:11:15 -0300

    Update 26082025

--- Back/application/models/Mercadolibre_model.php
+++ Back/application/models/Mercadolibre_model.php
@@ -359,10 +359,10 @@ class Mercadolibre_model extends CI_Model
 			$response_decode = json_decode(json_encode($rs_decode));
 			$rs_decoded = json_decode($response, true);
 			log_message('info', 'MP Response Status		         : ' . $httpcode);
-			log_message('info', 'MP Response                              : ');
-			log_message('info', 'MP status		                : ' . $rs_decoded['status']);
-			log_message('info', 'MP status_detail		                : ' . $rs_decoded['status_detail']);
-			log_message('info', print_r($response_decode, true));
+			log_message('info', 'MP status		                 : ' . $rs_decoded['status']);
+			log_message('info', 'MP status_detail		                 : ' . $rs_decoded['status_detail']);
+			log_message('info', 'MP Response                               : ');
+			log_message('info', print_r($rs_decode, true));
 			curl_close($curl);
 			if ($httpcode == 200 && ($response_decode->status == "approved" && $response_decode->status_detail == "accredited")) {
 				$lastPaymentAddedQuery = null;
