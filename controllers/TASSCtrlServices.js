cdca3b4b5f0bb6af050ac8add41e23ff0d946c09
David <rexx84@gmail.com>
2026-02-06 16:04:40 -0300

    Update 05022026

--- controllers/TASSCtrlServices.js
+++ controllers/TASSCtrlServices.js
@@ -1883,9 +1883,10 @@ services.controller('ServicesCtrl', function($scope, $location, $q, DateService,
                         }
                     }
                     $scope.customerFound.IsInDebtTmp=obj.IsInDebt==1?true:false;
-                    $scope.customerFound.isStockInBuildingTmp=obj.isStockInBuilding==1?true:false;
-                    $scope.customerFound.isStockInOfficeTmp=obj.isStockInOffice==1?true:false;
-                    $scope.customerFound.servicesStatustURL = serverHost+"/status/services/"+$scope.customerFound.idClient;
+                    $scope.customerFound.isStockInBuildingTmp   = obj.isStockInBuilding==1?true:false;
+                    $scope.customerFound.isStockInOfficeTmp     = obj.isStockInOffice==1?true:false;
+                    $scope.customerFound.servicesStatustURL     = serverHost+"/status/services/"+$scope.customerFound.idClient;
+                    $scope.customerFound.infoURL                = serverHost+"/info/client/"+$scope.customerFound.idClient;
                     console.log($scope.customerFound.idClient);
                     var zonaInfo=$scope.getZoneNameFn($scope.customerFound.idZonaFk);
                     $scope.getContractsByCustomerIdFn($scope.customerFound.idClient,'assign');
