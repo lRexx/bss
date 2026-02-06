4da436b1b3af0784178f6555583dd5d62d19e1b8
David <rexx84@gmail.com>
2026-02-06 16:16:04 -0300

    Update 06022026

--- controllers/TASSCtrlMonitor.js
+++ controllers/TASSCtrlMonitor.js
@@ -2519,6 +2519,7 @@ monitor.controller('MonitorCtrl', function($scope, $rootScope, $http, $location,
                       });
                       $scope.update.ticket.idTicket              = obj.selected.idTicket;
                       $scope.update.ticket.idNewStatusKf         = "8";
+                      $scope.update.ticket.idTypeTicketKf        = obj.selected.idTypeTicketKf;
                       $scope.update.ticket.delivery_schedule_at  = null;
                       $scope.update.ticket.delivered_at          = null
                       $scope.update.ticket.history               = [];
