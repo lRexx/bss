5325a927ec495c06e08f58b197623abe5bd634be
David <rexx84@gmail.com>
2026-03-19 09:21:31 -0300

    Update 19032026

--- controllers/TASSCtrlServices.js
+++ controllers/TASSCtrlServices.js
@@ -5204,6 +5204,7 @@ services.controller('ServicesCtrl', function($scope, $location, $q, DateService,
                         $scope.service.adicional={};
                         $scope.productSelected=obj;
                         console.log(obj);
+                        console.log($scope.service.new);
                         $scope.service.adicional.idProductoFk=$scope.productSelected.idProduct;
                         $scope.productDetailsAssigned=false;
                         if (($scope.service.new!=undefined && $scope.service.new.idTipeServiceFk!='3' && $scope.service.new.idTipeServiceFk!='4' && $scope.service.new.idTipeServiceFk!='5') || ($scope.service.update!=undefined && $scope.service.update.idTipeServiceFk!='3' && $scope.service.update.idTipeServiceFk!='4' && $scope.service.update.idTipeServiceFk!='5')){
