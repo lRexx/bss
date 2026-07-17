159cb9eea66c720f127b8fc1622493916ed9a2a4
David <rexx84@gmail.com>
2026-07-16 23:42:05 -0300

    Update 16072026

--- controllers/TASSCtrlMenu.js
+++ controllers/TASSCtrlMenu.js
@@ -990,6 +990,44 @@
             });
             //console.log($scope.profile);
           }
+          $scope.updateProfileLoggedUser=function(){
+            $scope.rsUser = {'user':{}}
+            var isEmailChange = $scope.sysLoggedUser.emailUser != $scope.profile.emailUser?true:false;
+            console.log("==========================================");
+                $scope.sysLoggedUser.fullNameUser         = $scope.profile.fullNameUser;
+                $scope.sysLoggedUser.emailUser            = $scope.profile.emailUser;
+                $scope.sysLoggedUser.phoneNumberUser      = $scope.normalizePhoneE164($scope.select.phoneCountryMovil.selected,$scope.profile.phoneMovilPrefixNumber,$scope.profile.phoneMovilNumberUser);
+                $scope.sysLoggedUser.phoneLocalNumberUser = $scope.normalizePhoneE164($scope.select.phoneCountryWired.selected,$scope.profile.phoneLocalPrefixNumber,$scope.profile.phoneLocalNumberUser);
+                $scope.sysLoggedUser.isEdit               = 1;
+                $scope.rsUser.user=$scope.sysLoggedUser;
+                $scope.rsUser.user.isEmailChange = isEmailChange;
+            console.log($scope.rsUser)
+            console.log("==========================================");
+            userServices.updateUser($scope.rsUser).then(function(response){
+              if(response.status==200){
+                console.log("Usuario: "+$scope.rsUser.user.fullNameUser+" Successfully updated");
+                inform.add($scope.rsUser.user.fullNameUser+', su perfil ha sido actualizado con exito. ',{
+                      ttl:4000, type: 'success'
+                });
+                if ($scope.rsUser.user.isEmailChange){
+                  $('#showModalEmailChange').modal({backdrop: 'static', keyboard: false});
+                }
+                $('#ProfileModalUser').modal('hide');
+                $timeout(function() {
+                  $scope.updateSysUserLoggedSession($scope.rsUser.user.idUser);
+                }, 1000);
+              }else if(response.status==404){
+                inform.add('[Error]: '+response.status+', Ocurrio error verifique los datos e intenta de nuevo o contacta el area de soporte. ',{
+                  ttl:5000, type: 'danger'
+                  });
+              }else if(response.status==500){
+                console.log("User not updated, contact administrator");
+                inform.add('[Error]: '+response.status+', Ha ocurrido un error en la comunicacion con servidor, contacta el area de soporte. ',{
+                  ttl:5000, type: 'danger'
+                  });
+              }
+            });
+          }
           $scope.normalizePhoneE164 = function (countryCodeTmp, prefixNumber, phoneNumber) {
 
               if (!countryCodeTmp ||
@@ -1120,44 +1158,6 @@
                   phoneNumber: phoneNumber
               };
           };
-          $scope.updateProfileLoggedUser=function(){
-            $scope.rsUser = {'user':{}}
-            var isEmailChange = $scope.sysLoggedUser.emailUser != $scope.profile.emailUser?true:false;
-            console.log("==========================================");
-                $scope.sysLoggedUser.fullNameUser         = $scope.profile.fullNameUser;
-                $scope.sysLoggedUser.emailUser            = $scope.profile.emailUser;
-                $scope.sysLoggedUser.phoneNumberUser      = $scope.normalizePhoneE164($scope.select.phoneCountryMovil.selected,$scope.profile.phoneMovilPrefixNumber,$scope.profile.phoneMovilNumberUser);
-                $scope.sysLoggedUser.phoneLocalNumberUser = $scope.normalizePhoneE164($scope.select.phoneCountryWired.selected,$scope.profile.phoneLocalPrefixNumber,$scope.profile.phoneLocalNumberUser);
-                $scope.sysLoggedUser.isEdit               = 1;
-                $scope.rsUser.user=$scope.sysLoggedUser;
-                $scope.rsUser.user.isEmailChange = isEmailChange;
-            console.log($scope.rsUser)
-            console.log("==========================================");
-            userServices.updateUser($scope.rsUser).then(function(response){
-              if(response.status==200){
-                console.log("Usuario: "+$scope.rsUser.user.fullNameUser+" Successfully updated");
-                inform.add($scope.rsUser.user.fullNameUser+', su perfil ha sido actualizado con exito. ',{
-                      ttl:4000, type: 'success'
-                });
-                if ($scope.rsUser.user.isEmailChange){
-                  $('#showModalEmailChange').modal({backdrop: 'static', keyboard: false});
-                }
-                $('#ProfileModalUser').modal('hide');
-                $timeout(function() {
-                  $scope.updateSysUserLoggedSession($scope.rsUser.user.idUser);
-                }, 1000);
-              }else if(response.status==404){
-                inform.add('[Error]: '+response.status+', Ocurrio error verifique los datos e intenta de nuevo o contacta el area de soporte. ',{
-                  ttl:5000, type: 'danger'
-                  });
-              }else if(response.status==500){
-                console.log("User not updated, contact administrator");
-                inform.add('[Error]: '+response.status+', Ha ocurrido un error en la comunicacion con servidor, contacta el area de soporte. ',{
-                  ttl:5000, type: 'danger'
-                  });
-              }
-            });
-          }
         /**************************************************
         *                                                 *
         *        REMOVER TENANT DE UN DEPARTAMENTO        *
