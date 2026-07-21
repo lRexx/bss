4d6dcf7f1e739bd72778221c639b8f687b365348
David <rexx84@gmail.com>
2026-07-20 23:56:44 -0300

    Update 20072026

--- controllers/TASSCtrlTickets.js
+++ controllers/TASSCtrlTickets.js
@@ -3675,8 +3675,8 @@ tickets.controller('TicketsCtrl', function($scope, $compile, $location, $interva
                             $('#dniUser').focus();
                             $scope.select.phoneCountryWired.selected    = $scope.countryPhoneCodesList.find(c => c.isoCode === "AR");
                             $scope.select.phoneCountryMovil.selected    = $scope.countryPhoneCodesList.find(c => c.isoCode === "AR");
-                            $scope.users.new.phonelocalPrefixNumber     = "11"
-                            $scope.users.new.phoneMovilPrefixNumber     = "11"
+                            $scope.tenant.new.phonelocalPrefixNumber    = "11"
+                            $scope.tenant.new.phoneMovilPrefixNumber    = "11"
                         });
                         console.log($scope.ticket);
                         console.log($scope.tenant.new);
