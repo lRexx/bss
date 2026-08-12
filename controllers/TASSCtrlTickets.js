9054a7a4c63a7f4f67dc630a9b8128eaa5233574
David <rexx84@gmail.com>
2026-08-11 23:52:40 -0300

    Update 11082026

--- controllers/TASSCtrlTickets.js
+++ controllers/TASSCtrlTickets.js
@@ -2263,7 +2263,14 @@ tickets.controller('TicketsCtrl', function($scope, $compile, $location, $interva
 
             $scope.filterByInitialDelivery = function(item) {
                 //console.log("Filtering item:", item);
-                // If no radio button is selected, show all items (or return false to show none)
+                console.log("$scope.ticket.idDeviceTypeKf                   : ", $scope.ticket.idDeviceTypeKf);
+                console.log("item.idTypeTenant                              : ", item.idTypeTenant);
+                console.log("$scope.ticket.building.isInitialDeliveryActive : ", $scope.ticket.building.isInitialDeliveryActive);
+                console.log("$scope.ticket.building.expiration_state        : ", $scope.ticket.building.expiration_state);
+                console.log("--------------------------------------------");
+                console.log($scope.ticket ? 'Ticket is defined' : 'Ticket is undefined');
+                console.log($scope.ticket.building ? 'Ticket building is defined' : 'Ticket building is undefined');
+                console.log("--------------------------------------------");
                 if ($scope.ticket.idDeviceTypeKf=="2" && item.idTypeTenant!=1 && $scope.ticket && $scope.ticket.building && $scope.ticket.building.isInitialDeliveryActive && !$scope.ticket.building.expiration_state) {
                     return item;
                 }else if ($scope.ticket.idDeviceTypeKf=="2" && $scope.ticket && $scope.ticket.building && !$scope.ticket.building.isInitialDeliveryActive && $scope.ticket.building.expiration_state) {
