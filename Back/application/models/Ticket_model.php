1cc7d6f63942f5af94c708840edb1748e646ce03
David <rexx84@gmail.com>
2026-08-20 23:41:12 -0300

    Update 20082026

--- Back/application/models/Ticket_model.php
+++ Back/application/models/Ticket_model.php
@@ -3312,7 +3312,7 @@ class Ticket_model extends CI_Model
 				if (@$data['idStatusTicketKf'] == '10') {
 					$where = "isCancelRequested = 1 AND idStatusTicketKf = " . @$data['idStatusTicketKf'];
 					$this->db->where($where);
-				} else if (@$data['idDeviceTypeKf'] != '' && @$data['idDeviceTypeKf'] != null && @$data['idDeviceTypeKf'] != '2' && @$data['idStatusTicketKf'] == '8' && @$data['idMgmtMethodKf'] == null && @$data['whereKeysAreEnable'] == null && @$data['isKeysEnable'] == null) {
+				} else if (((@$data['idDeviceTypeKf'] != '' && @$data['idDeviceTypeKf'] != '2') || (@$data['idDeviceTypeKf'] == null)) && @$data['idStatusTicketKf'] == '8' && @$data['idMgmtMethodKf'] == null && @$data['whereKeysAreEnable'] == null && @$data['isKeysEnable'] == null) {
 					$where = "ISNULL(idMgmtMethodKf) AND ISNULL(whereKeysAreEnable) AND ISNULL(isKeysEnable) AND idStatusTicketKf = " . @$data['idStatusTicketKf'];
 					$this->db->where($where);
 				} else {
